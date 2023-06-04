d7eae26107500af187c6c532575e383b09777f77
LuizMafraJNR <luiz.sesna@univille.br>
2023-06-04 14:39:12 -0300

    Alterando o application properties de acordo com o profile.

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmailMOCK.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmailMOCK.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 @Profile("Developer")
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmailMOCK implements Notificador {
+
+    @Autowired
+    private NotificadorProperties notificadorProperties;
 
     public NotificadorEmailMOCK() {
         System.out.println("NotificadorEmail MOCK.");
@@ -15,6 +19,8 @@ public class NotificadorEmailMOCK implements Notificador {
     @Override
     public void notificar(Cliente cliente, String mensagem){
         System.out.printf("MOCK: Notificando %s atraves do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.println("Servidor email: "+notificadorProperties.getHostServidor());
+        System.out.println("Porta servidor: "+notificadorProperties.getPorta());
     }
 
 }
