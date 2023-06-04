cd598e256b2f397fe830c45ae0d769a2ae8b121b
luiz.junior <luiz.junior@neomind.com.br>
2023-06-03 22:31:13 -0300

    Utilizando o @Autowired para injetar dependecias e exclusão das classes de config para injetar.

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,26 +1,14 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.model.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NotificadorEmail implements Notificador {
-    private boolean caixaAlta;
-    private String hostServidorSmtp;
-    public NotificadorEmail(String hostServidorSmtp){
-        /**
-         * Tem alguns momentos que a gente quer editar a instanciação do bean, não simplesmente dar um new Instace.
-         * Criar um novo bean e deixar disponivel no container do Spring.
-         */
-        this.hostServidorSmtp = hostServidorSmtp;
-        System.out.println("Notificador Email");
-    }
+
     @Override
     public void notificar(Cliente cliente, String mensagem){
-        if (this.caixaAlta){
-            mensagem = mensagem.toUpperCase();
-        }
         System.out.printf("Notificando %s atraves do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
-    }
 }
--- src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -2,19 +2,32 @@ package com.algaworks.algafoodapi.di.service;
 
 import com.algaworks.algafoodapi.di.model.Cliente;
 import com.algaworks.algafoodapi.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
+    @Autowired // 3 forma
     private Notificador notificador;
+//
+//    @Autowired
+     // Primeira forma pelo construtor (AutoWired opcional)
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//        System.out.println("Ativação" + notificador);
+//    }
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-        System.out.println("Ativação" + notificador);
-    }
+//    public AtivacaoClienteService(String qualquer){
+//
+//    }
 
     public void ativar(Cliente cliente){
         cliente.setAtivo(true);
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo.");
     }
 
-
+//    @Autowired // Segunda forma
+//    public void setNotificador(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 }
