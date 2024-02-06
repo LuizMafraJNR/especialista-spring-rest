594a8e908d914c145f694698c1e8fffb09835a15
LuizMafraJNR <luiz.sesna@univille.br>
2024-02-05 21:39:19 -0300

    Desafio: Modelando e implementando a atualização de recursos de restaurantes.

--- src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
@@ -1,10 +1,12 @@
 package com.algaworks.algafoodapi.api.controller;
 
 import com.algaworks.algafoodapi.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.model.Restaurante;
 import com.algaworks.algafoodapi.domain.repository.RestauranteRepository;
 import com.algaworks.algafoodapi.domain.service.CadastroRestauranteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,6 +42,23 @@ public class RestauranteController {
             restaurante = restauranteService.adicionar(restaurante);
             return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
         } catch (EntidadeEmUsoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable Long id,@RequestBody Restaurante restaurante){
+        try {
+            Restaurante restauranteAtual = restauranteRepository.buscarId(id);
+            if (restauranteAtual == null){
+                return ResponseEntity.notFound().build();
+            }
+
+            BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+            restauranteAtual = restauranteService.adicionar(restauranteAtual);
+            return ResponseEntity.ok(restauranteAtual);
+
+        } catch (EntidadeNaoEncontradaException e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
