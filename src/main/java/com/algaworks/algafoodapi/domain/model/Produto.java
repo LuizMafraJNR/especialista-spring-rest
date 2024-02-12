06bc19e2cd87928899f9685bab63ac9e9cd4a1d1
LuizMafraJNR <luiz.sesna@univille.br>
2024-02-12 16:12:33 -0300

    Desafio: mapeando relacionamento muitos-para-um

---
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Produto.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafoodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class Produto {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String descricao;
+    @Column(nullable = false)
+    private BigDecimal preco;
+    @Column(nullable = false)
+    private Boolean ativo;
+    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
+}
