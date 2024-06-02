179a5b209c9768c791e243be8c2ea3dfe34c15ad
LuizMafraJNR <luiz.sesna@univille.br>
2024-06-01 23:37:30 -0300

    RFC 7807

--- src/main/java/com/algaworks/algafoodapi/api/controller/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/exceptionhandler/ApiExceptionHandler.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+// RFC 7807 PROBLEM DETAILS HTTP APIs
+
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler
 {
