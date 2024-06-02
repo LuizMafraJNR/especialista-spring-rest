da30d71c047e27b46164d4f3548ab4404ac77a8b
LuizMafraJNR <luiz.sesna@univille.br>
2024-06-01 23:26:01 -0300

    Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafoodapi/api/controller/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/exceptionhandler/ApiExceptionHandler.java
@@ -4,46 +4,53 @@ import com.algaworks.algafoodapi.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafoodapi.domain.exception.NegocioException;
 import java.time.LocalDateTime;
+import java.util.Objects;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler
 {
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEstadoNaoEncontradoException(EntidadeNaoEncontradaException e)
+	public ResponseEntity<?> tratarEstadoNaoEncontradoException(EntidadeNaoEncontradaException e, WebRequest request)
 	{
-		Problem problema = Problem.builder()
-			.dateTime(LocalDateTime.now())
-			.message(e.getMessage())
-			.build();
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-			.body(problema);
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException()
+	public ResponseEntity<?> tratarEntidadeEmUsoException(WebRequest request, EntidadeEmUsoException e)
 	{
-		Problem problema = Problem.builder()
-			.dateTime(LocalDateTime.now())
-			.message("Entidade está sendo utilizada por outra entidade, por isso não pode ser removida")
-			.build();
-		return ResponseEntity.status(HttpStatus.CONFLICT)
-			.body(problema);
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
 	}
 
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException e)
+	public ResponseEntity<?> tratarNegocioException(NegocioException e, WebRequest request)
 	{
-		Problem problema = Problem.builder()
-			.dateTime(LocalDateTime.now())
-			.message(e.getMessage())
-			.build();
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(problema);
+	    return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+		HttpHeaders headers, HttpStatus status, WebRequest request)
+	{
+		if(Objects.isNull(body))
+		{
+			body = Problem.builder()
+				.dateTime(LocalDateTime.now())
+				.message(status.getReasonPhrase())
+				.build();
+		} else if (body instanceof String)
+		{
+			body = Problem.builder()
+				.dateTime(LocalDateTime.now())
+				.message((String) body)
+				.build();
+		}
+		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 }
