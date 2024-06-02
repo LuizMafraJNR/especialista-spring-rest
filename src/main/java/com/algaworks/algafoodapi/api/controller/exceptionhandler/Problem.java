7c863613d1319337e97ee16fcb0624283f48c098
LuizMafraJNR <luiz.sesna@univille.br>
2024-06-01 23:55:01 -0300

    Padronizando o formato de problemas no corpo de respostas com a RFC 7807

--- src/main/java/com/algaworks/algafoodapi/api/controller/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/exceptionhandler/Problem.java
@@ -1,13 +1,18 @@
 package com.algaworks.algafoodapi.api.controller.exceptionhandler;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Data;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @Builder
 public class Problem
 {
+	private Integer status;
+	private String type;
+	private String title;
+	private String detail;
 	private LocalDateTime dateTime;
-	private String message;
 }
