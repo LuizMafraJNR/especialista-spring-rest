b3739af4ff524fe07da563da4b7789529590a923
luiz.junior <luiz.junior@neomind.com.br>
2025-05-16 00:20:22 -0300

    Aula 11.16. Customizando o mapeamento de propriedades com ModelMapper. Onde não bata todas as correspondencias/tokens.

--- src/main/java/com/algaworks/algafoodapi/api/model/RestauranteDTO.java
+++ src/main/java/com/algaworks/algafoodapi/api/model/RestauranteDTO.java
@@ -10,7 +10,7 @@ public class RestauranteDTO
 {
 	private Long id;
 	private String nome;
-	private BigDecimal frete;
+	private BigDecimal precoFrete;
 	private CozinhaDTO cozinha;
 
 }
--- src/main/java/com/algaworks/algafoodapi/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafoodapi.core.modelmapper;
 
+import com.algaworks.algafoodapi.api.model.RestauranteDTO;
+import com.algaworks.algafoodapi.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +12,10 @@ public class ModelMapperConfig
 	@Bean
 	public ModelMapper modelMapper()
 	{
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		modelMapper.createTypeMap(Restaurante.class, RestauranteDTO.class)
+			.addMapping(Restaurante::getTaxaFrete, RestauranteDTO::setPrecoFrete);
+		return modelMapper;
 	}
 
 }
