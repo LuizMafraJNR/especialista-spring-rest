0f7cf1f0e1a4e139605593c950cad1daa7abb820
LuizMafraJNR <luiz.sesna@univille.br>
2025-11-09 21:57:11 -0300

    12.10. Um pouco mais sobre JPA: objeto alterado fora da transação é sincronizado com o banco de dados

--- src/main/java/com/algaworks/algafoodapi/domain/service/UsuarioService.java
+++ src/main/java/com/algaworks/algafoodapi/domain/service/UsuarioService.java
@@ -14,9 +14,20 @@ public class UsuarioService
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
+	//	aqui ele vai salvar no banco de dados por conta do contexto de persistência
+	/*
+	Então por mais que fazemos alteração por fora da transição, quando o método transacional terminar ele vai fazer o flush
+	da entidade que está no contexto de persistência
+	é por isso que não precisamos chamar o save do repository
+	é uma funcionalidade do JPA chamada de dirty checking
+	é uma funcionalidade que monitora as entidades que estão no contexto de persistência
+	quando o método transacional termina ele verifica se houve alguma alteração nessas entidades
+	*/
 	@Transactional
-	public Usuario salvar(Usuario usuario) {
-		return usuarioRepository.save(usuario);
+	public Usuario salvar(Usuario usuario)
+	{
+		//return usuarioRepository.save(usuario);
+		return usuario;
 	}
 
 	@Transactional
